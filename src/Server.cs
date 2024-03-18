28bdc667cd94e6b3a398534d16cf7345e4a3f0b8
davidbetteridge <david.betteridge@zeromission.io>
2024-03-18 20:52:21 +0000

    pass 2nd stage

--- src/Server.cs
+++ src/Server.cs
@@ -7,4 +7,10 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+var socket = server.AcceptSocket(); // wait for client
+
+var buffer = new byte[1024];
+socket.Receive(buffer);
+
+var bytes = System.Text.Encoding.UTF8.GetBytes("+PONG\r\n");
+socket.Send(bytes);
