6282d7d2926200afaeb587e07fe9492d3ed430f9
davidbetteridge <david.betteridge@zeromission.io>
2024-03-18 22:30:36 +0000

    <your_commit_message>

--- src/EventLoop.cs
+++ src/EventLoop.cs
@@ -20,23 +20,33 @@ public class EventLoop
             {
                 var socket = socketAndCommand.Item1;
                 var command = socketAndCommand.Item2;
-                
-                if (command[0] == "ping")
+                var commandType = command[0].ToLowerInvariant();
+                if (commandType == "ping")
                 {
                     Console.WriteLine("Pong " + socket.Handle );
-                    var bytes = System.Text.Encoding.UTF8.GetBytes("+PONG\r\n");
+                    var bytes = System.Text.Encoding.UTF8.GetBytes(SimpleString("PONG"));
                     socket.Send(bytes);                    
                 }
-                else if (command[0] == "echo")
+                else if (commandType == "echo")
                 {
                     Console.WriteLine("Echo " + socket.Handle );
-                    var bytes = System.Text.Encoding.UTF8.GetBytes($"${command[1].Length}\r\n{command[1]}\r\n");
+                    var bytes = System.Text.Encoding.UTF8.GetBytes(BulkString(command[1]));
                     socket.Send(bytes);                    
                 }
 
+                else if (commandType == "set")
+                {
+                    Console.WriteLine($"set " + socket.Handle + ":: " + command[1] + " => " + command[2] );
+                    var bytes = System.Text.Encoding.UTF8.GetBytes(SimpleString("OK"));
+                    socket.Send(bytes);                    
+                }
+                
             }   
             
             Thread.Sleep(100);
         }
     }
+
+    private string SimpleString(string str) => $"+{str}\r\n";
+    private string BulkString(string str) => $"${str.Length}\r\n{str}\r\n";
 }
