57fa6533f4b02ca9ef6feadfc6b7950c6762ee15
davidbetteridge <david.betteridge@zeromission.io>
2024-03-18 22:35:08 +0000

    <your_commit_message>

--- src/EventLoop.cs
+++ src/EventLoop.cs
@@ -7,6 +7,8 @@ public class EventLoop
 {
     private readonly ConcurrentQueue<(Socket, string[])> _commandQueue = new();
     
+    private Dictionary<string, string> _cache = new ();
+    
     public void AddCommand(Socket socket, string[] command)
     {
         _commandQueue.Enqueue((socket, command));
@@ -37,8 +39,26 @@ public class EventLoop
                 else if (commandType == "set")
                 {
                     Console.WriteLine($"set " + socket.Handle + ":: " + command[1] + " => " + command[2] );
+                    _cache[command[1]] = command[2];
                     var bytes = System.Text.Encoding.UTF8.GetBytes(SimpleString("OK"));
                     socket.Send(bytes);                    
+                }
+                
+                else if (commandType == "get")
+                {
+                    Console.WriteLine($"get " + socket.Handle + ":: " + command[1] );
+                    if (_cache.TryGetValue(command[1], out var value))
+                    {
+                        var bytes = System.Text.Encoding.UTF8.GetBytes(BulkString(value));
+                        socket.Send(bytes);      
+                    }
+                    else
+                    {
+                        var bytes = System.Text.Encoding.UTF8.GetBytes(NullBulkString);
+                        socket.Send(bytes);
+                    }
+                    
+                 
                 }
                 
             }   
@@ -49,4 +69,5 @@ public class EventLoop
 
     private string SimpleString(string str) => $"+{str}\r\n";
     private string BulkString(string str) => $"${str.Length}\r\n{str}\r\n";
+    private const string NullBulkString = "$-1\r\n";
 }
